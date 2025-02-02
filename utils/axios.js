62c95cb64c0359e1e1e320e8817992b764cdde8f
ROMANVIKI <vikiadhi2016@gmail.com>
2025-02-03 00:58:31 +0530

    axios conf

--- utils/axios.js
+++ utils/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-// Axios instance with a base URL
-
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const instance = axios.create({
   baseURL: apiUrl,
+  withCredentials: true, // Enable sending cookies & credentials
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default instance;
