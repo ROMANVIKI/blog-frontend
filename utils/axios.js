b2fda4c424f491ff8509a09013e19656561d562b
ROMANVIKI <vikiadhi2016@gmail.com>
2025-02-02 12:45:38 +0530

    added baseurl

--- utils/axios.js
+++ utils/axios.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 // Axios instance with a base URL
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: apiUrl,
 });
 
 export default instance;
