db122b8062e82194af732f77617335ea5a930c57
ROMANVIKI <vikiadhi2016@gmail.com>
2025-03-07 15:59:46 +0530

    added hs for lang

--- components/Tiptap/BlogViewer.jsx
+++ components/Tiptap/BlogViewer.jsx
@@ -1,9 +1,34 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 import DOMPurify from "dompurify";
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
 import "./styles.scss";
 
 const BlogViewer = memo(({ content }) => {
   const sanitizedContent = DOMPurify.sanitize(content);
+
+  useEffect(() => {
+    document.querySelectorAll("pre code").forEach((block) => {
+      hljs.highlightElement(block);
+    });
+
+    document.querySelectorAll("pre").forEach((block) => {
+      if (block.querySelector(".copy-button")) return;
+
+      const copyButton = document.createElement("button");
+      copyButton.classList.add("copy-button");
+      copyButton.innerText = "Copy";
+      block.appendChild(copyButton);
+
+      copyButton.addEventListener("click", () => {
+        const code = block.querySelector("code").innerText;
+        navigator.clipboard.writeText(code).then(() => {
+          copyButton.innerText = "Copied!";
+          setTimeout(() => (copyButton.innerText = "Copy"), 1500);
+        });
+      });
+    });
+  }, [content]);
 
   return (
     <div
